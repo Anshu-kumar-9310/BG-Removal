dbca5ff11fd288c516c93c53ea69d06f65fbe98d
Anshu-kumar-9310 <anshuji9310@gmail.com>
2024-11-02 11:33:10 +0530

    error resove

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { clerkWebhooks } from '../controllers/UserController'
+import { clerkWebhooks } from '../controllers/UserController.js'
+
 
 
 const userRouter = express.Router()
 
-userRouter.post('/webhooks', clerkWebhooks)
+userRouter.post('/webhooks',clerkWebhooks)
 
 export default userRouter
